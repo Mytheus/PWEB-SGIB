2b3e6541ca94cafb2a2b1be2224996849a9258b2
José Matheus <matheussantana2603@gmail.com>
2022-01-23 23:13:36 -0300

    Apagando comentários

--- sgib/apps/biblioteca/forms.py
+++ sgib/apps/biblioteca/forms.py
@@ -3,10 +3,8 @@ from sgib.apps.biblioteca.models import Livro
 
 class LivroForm(forms.ModelForm):
     titulo = forms.CharField(label="Título*")
-    #genero = forms.CharField(label='Genero*', widget=forms.Select(choices=Livro.GENEROS))
     autor = forms.CharField(label="Autor*")
     editora = forms.CharField(label="Editora*")
-    #descricao = forms.CharField(label="Descrição")
     disponivel = forms.CheckboxInput()
 
     class Meta:
